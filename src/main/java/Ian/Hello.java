38ea2abe1746597b768a722e4e360f174ea5e25d
Ian Pan <ian8832@gmail.com>
2020-04-24 14:05:50 +0800

    Initial commit

---
+++ src/main/java/Ian/Hello.java
@@ -0,0 +1,8 @@
+package Ian;
+
+public class Hello {
+        public static void main(String[] args) {
+                Say a = new Say();
+                a.hello();
+        }
+}
---
+++ src/main/java/Ian/Say.java
@@ -0,0 +1,7 @@
+package Ian;
+
+public class Say {
+    public void hello() {
+        System.out.println("hello!!");
+    }
+}
